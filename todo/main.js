4cd0e3fb72554bc1391e91e4c91c9ba97c20ef38
KiselevaOlga <jjcafe09@gmail.com>
2021-03-02 21:47:07 +0000

    small changes in function main.js

--- todo/main.js
+++ todo/main.js
@@ -1,11 +1,21 @@
 let addNewTask = document.getElementById("add_task");
 let taskText = document.getElementById("task_text");
+// let itemList = document.getElementById("my_list");
+
 
 addNewTask.addEventListener('click', function(){
     let addNew = document.createElement('li');
     addNew.innerHTML = taskText.value;
-    console.log('keys: ', Object.keys(taskText));
     document.getElementById("my_list").appendChild(addNew);
+    taskText.value = '';
+    addNew.addEventListener('click', function(){
+        addNew.style.textDecoration = 'line-through'
+        addNew.style.backgroundColor = 'red'
+    })
+    addNew.addEventListener('mouseover', function(){
+        addNew.style.backgroundColor = ''
+    })
 })
 
-console.log('connected', addNewTask, Object.keys(taskText));
+
+console.log('connected', document.getElementsByTagName('LI'));
