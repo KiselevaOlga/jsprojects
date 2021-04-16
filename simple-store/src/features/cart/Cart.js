7e068b8eded7b9f2d9d8b8ddb604c9c6340a472e
KiselevaOlga <jjcafe09@gmail.com>
2021-04-16 11:45:57 +0100

    finished simple store project

--- simple-store/src/features/cart/Cart.js
+++ simple-store/src/features/cart/Cart.js
@@ -1,51 +1,59 @@
 import React from 'react';
-import {calculateTotal, getCurrencySymbol} from '../../utilities/utilities.js';
+import {calculateTotal,getCurrencySymbol,} from '../../utilities/utilities.js';
 import {changeItemQuantity} from './cartSlice.js';
 
-export const Cart = (props)=>{
-    const {cart, dispatch, currencyFilter} = props;
+export const Cart = (props) => {
+  const { cart, currencyFilter, dispatch } = props;
 
-    const onInputChangeHandler = (name, input)=>{
-        const newQuantity = Number(input);
-        if (input ===''){
-            return;
-        }
-        dispatch(changeItemQuantity(name, newQuantity))
+  const onInputChangeHandler = (name, input) => {
+    if (input === '') {
+      return;
     }
-    const cartElements= Object.keys(cart).map(cartItem=>createCartItem(cartItem))
 
-    const createCartItem = (name)=>{
-        const item = cart[name];
-        if(item.quantity===0){
-            return;
-        }
-        return (
-            <li key={name}>
-                <p>{name}</p>
-                <select
-                    className='item-quantity'
-                    value={item.quantity}
-                    onChange={event=>onInputChangeHandler(name, event.target.value)}
-                >
-                    {[...Array(100).keys()].map((_, index)=>(
-                        <option key={index} value={index}>
-                            {index}
-                        </option>
-                    ))}                    
-                </select>
-            </li>
-        )
+    const newQuantity = Number(input);
+
+    dispatch(changeItemQuantity(name, newQuantity))
+  };
+
+  
+  const cartElements = Object.keys(cart).map(item=>createCartItem(item))
+  const total = calculateTotal(cart, currencyFilter);
+  return (
+    <div id="cart-container">
+      <ul id="cart-items">{cartElements}</ul>
+      <h3 className="total">
+        Total{' '}
+        <span className="total-value">
+          {getCurrencySymbol(currencyFilter)}{total} {currencyFilter}
+        </span>
+      </h3>
+    </div>
+  );
+
+  function createCartItem(name) {
+    const item = cart[name];
+
+    if (item.quantity === 0) {
+      return;
     }
-    const total = calculateTotal(cart, currencyFilter)
+
     return (
-        <div id='cart-container'>
-            <ul id='cart-items'>{cartElements}</ul>
-            <h3 className='total'>Total: {' '}
-            <span className='total-value'>
-                {getCurrencySymbol(currencyFilter)}{total}{currencyFilter}
-            </span>
-            </h3>
-        </div>
-    )
-    
-}
+      <li key={name}>
+        <p>{name}</p>
+        <select
+          className="item-quantity"
+          value={item.quantity}
+          onChange={(e) => {
+            onInputChangeHandler(name, e.target.value);
+          }}
+        >
+          {[...Array(100).keys()].map((_, index) => (
+            <option key={index} value={index}>
+              {index}
+            </option>
+          ))}
+        </select>
+      </li>
+    );
+  }
+};
